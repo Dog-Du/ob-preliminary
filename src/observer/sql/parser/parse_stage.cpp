7752cb62b1a8badf13230de115bca6c2c2fc9e82
Dog-Du <2976604925@qq.com>
2024-04-21 22:34:49 +0800

    date_test_2

--- src/observer/sql/parser/parse_stage.cpp
+++ src/observer/sql/parser/parse_stage.cpp
@@ -60,23 +60,6 @@ RC ParseStage::handle_request(SQLStageEvent *sql_event)
 
   // 枚举所有可能出现 日期字面量 的地方。因为在正则表达式的地方已经进行了筛选，
   switch (sql_node->flag) {
-    case SCF_SELECT: {
-      for (auto &it : sql_node->selection.conditions) {
-        if (it.left_value.attr_type() == DATES && it.left_is_attr == 0 && !check_date(it.left_value.get_date())) {
-          rc = RC::VARIABLE_NOT_VALID;
-          sql_result->set_return_code(rc);
-          sql_result->set_state_string("Error date");
-          return rc;
-        }
-
-        if (it.right_value.attr_type() == DATES && it.right_is_attr == 0 && !check_date(it.right_value.get_date())) {
-          rc = RC::VARIABLE_NOT_VALID;
-          sql_result->set_return_code(rc);
-          sql_result->set_state_string("Error date");
-          return rc;
-        }
-      }
-    } break;
     case SCF_INSERT: {
       for (auto &it : sql_node->insertion.values) {
         if (it.attr_type() == DATES && !check_date(it.get_date())) {
@@ -89,23 +72,6 @@ RC ParseStage::handle_request(SQLStageEvent *sql_event)
     } break;
     case SCF_UPDATE: {
       for (auto &it : sql_node->update.conditions) {
-        if (it.left_value.attr_type() == DATES && it.left_is_attr == 0 && !check_date(it.left_value.get_date())) {
-          rc = RC::VARIABLE_NOT_VALID;
-          sql_result->set_return_code(rc);
-          sql_result->set_state_string("Error date");
-          return rc;
-        }
-
-        if (it.right_value.attr_type() == DATES && it.right_is_attr == 0 && !check_date(it.right_value.get_date())) {
-          rc = RC::VARIABLE_NOT_VALID;
-          sql_result->set_return_code(rc);
-          sql_result->set_state_string("Error date");
-          return rc;
-        }
-      }
-    } break;
-    case SCF_DELETE: {
-      for (auto &it : sql_node->deletion.conditions) {
         if (it.left_value.attr_type() == DATES && it.left_is_attr == 0 && !check_date(it.left_value.get_date())) {
           rc = RC::VARIABLE_NOT_VALID;
           sql_result->set_return_code(rc);
