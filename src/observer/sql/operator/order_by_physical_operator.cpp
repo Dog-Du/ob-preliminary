efac245d035d3e752e72f42b3901469e6d777559
Dog-Du <2976604925@qq.com>
2024-05-16 16:07:42 +0800

    order-by-test-6

--- src/observer/sql/operator/order_by_physical_operator.cpp
+++ src/observer/sql/operator/order_by_physical_operator.cpp
@@ -86,7 +86,7 @@ RC OrderByPhysicalOperator::open(Trx *trx)
       return false;
     };
 
-    std::sort(tuples_.begin(), tuples_.end(), cmp);
+    std::stable_sort(tuples_.begin(), tuples_.end(), cmp);
   }
 
   i_ = -1;
